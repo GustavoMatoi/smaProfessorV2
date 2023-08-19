d78608766bae872d0e5b838b3834416b04b4085c
Gustavo Vaz Teixeira <87538302+GustavoMatoi@users.noreply.github.com>
2023-08-19 16:23:21 -0300

    Gráfico CIT diária mega-filtrado finalizado

--- componentes/TelasDeEvolucao/GraficosEvolutivos/EvolucaoCIT.js
+++ componentes/TelasDeEvolucao/GraficosEvolutivos/EvolucaoCIT.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from "react"
-import {Text, View, SafeAreaView, ScrollView, StyleSheet } from 'react-native'
+import React, {useState, useEffect, useCallback } from "react"
+import {Text, View, SafeAreaView, ScrollView, StyleSheet, FlatList } from 'react-native'
 import estilo from "../../estilo"
 import RadioBotao from "../../RadioBotao"
 import {VictoryChart, VictoryLine, VictoryTheme, VictoryVoronoiContainer, VictoryLabel, VictoryAxis} from "victory-native"
@@ -29,6 +29,7 @@ export default ({route, navigation}) => {
     const [mesSelecionado, setMesSelecionado] = useState('')
     const [mesInicial, setMesInicial] = useState(0)
     const [arrayFiltrado, setArrayFiltrado] = useState([{x: 0, y: 0}])
+    const [arrayParametroX, setArrayParametroX] = useState([])
     useEffect (() => {
       const unsubscribe = NetInfo.addEventListener(state => {
         setConexao(state.type === 'wifi' || state.type === 'cellular')
@@ -68,12 +69,10 @@ export default ({route, navigation}) => {
             }
             newArrayPse.push(doc.get('PSE.valor') * doc.get('duracao'))
             newArrayCitObj.push(citObjeto)
-            console.log(`CIT : ${doc.get('PSE.valor')}, ${doc.get('duracao')}`)
         });  
         setArrayPse(newArrayPse)
         setCarregandoDados(false)
         setCitObjetos(newArrayCitObj)
-        console.log('citObjetos', citObjetos)
     };
 
     useEffect(() => {
@@ -113,23 +112,23 @@ export default ({route, navigation}) => {
             diaDaSemana: diaDaSemana
           });
 
-          if(doc.get('mes') === 1){ arrayMesesAux.push(`Janeiro ${doc.get('ano')}`)}
-          if(doc.get('mes') === 2){ arrayMesesAux.push(`Fevereiro ${doc.get('ano')}`)}
-          if(doc.get('mes') === 3){ arrayMesesAux.push(`Março ${doc.get('ano')}`)}
-          if(doc.get('mes') === 4){ arrayMesesAux.push(`Abril ${doc.get('ano')}`)}
-          if(doc.get('mes') === 5){ arrayMesesAux.push(`Maio ${doc.get('ano')}`)}
-          if(doc.get('mes') === 6){ arrayMesesAux.push(`Junho ${doc.get('ano')}`)}
-          if(doc.get('mes') === 7){ arrayMesesAux.push(`Julho ${doc.get('ano')}`)}
-          if(doc.get('mes') === 8){ arrayMesesAux.push(`Agosto ${doc.get('ano')}`)}
-          if(doc.get('mes') === 9){ arrayMesesAux.push(`Setembro ${doc.get('ano')}`)}
-          if(doc.get('mes') === 10){ arrayMesesAux.push(`Outubro ${doc.get('ano')}`)}
-          if(doc.get('mes') === 11){ arrayMesesAux.push(`Novembro ${doc.get('ano')}`)}
-          if(doc.get('mes') === 12){ arrayMesesAux.push(`Dezembro ${doc.get('ano')}`)}
+          if(doc.get('mes') === 1){ arrayMesesAux.push({data:`Janeiro ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 2){ arrayMesesAux.push({data:`Fevereiro ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 3){ arrayMesesAux.push({data:`Março ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 4){ arrayMesesAux.push({data:`Abril ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 5){ arrayMesesAux.push({data:`Maio ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 6){ arrayMesesAux.push({data:`Junho ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 7){ arrayMesesAux.push({data:`Julho ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 8){ arrayMesesAux.push({data:`Agosto ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 9){ arrayMesesAux.push({data:`Setembro ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 10){ arrayMesesAux.push({data:`Outubro ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 11){ arrayMesesAux.push({data:`Novembro ${doc.get('ano')}`, dia: doc.get('dia')})}
+          if(doc.get('mes') === 12){ arrayMesesAux.push({data:`Dezembro ${doc.get('ano')}`, dia: doc.get('dia')})}
           
         });
         setArrayPseSemanal(newArrayPse);
         setCarregandoDados(false);
-        setArrayMeses([...new Set(arrayMesesAux)])
+        setArrayMeses(arrayMesesAux)
         
 
       };
@@ -173,11 +172,8 @@ export default ({route, navigation}) => {
         const citMensal = mesesObj[mesAno].reduce((acc, cit) => acc + cit, 0);
         arrayMensalTemporario.push(citMensal);
       });
-      console.log('arrayPse', arrayPse)
       setCitSemanal(arraySemanalTemporario);
       setCitMensal(arrayMensalTemporario);
-      console.log("Mensal: " + arrayMensalTemporario)
-      console.log("Semanais: " + arraySemanalTemporario)
 
     };
 
@@ -237,27 +233,88 @@ for(let i = 0; i < citSemanal.length; i++){
 const citSemanalMapeado = vetorContadorSemanas.map(i => `Sem. ${i}`)
 
 
-useEffect (() => {
-  for (let i = 0; i < citObjetos.length; i++) {
-    if(citObjetos[i].data == mesSelecionado){
-      setMesInicial(i)
-      break;
-    }
-  }
 
-}, [mesSelecionado])
 
 const handleSelectChange = (value) => {
   setMesSelecionado(value);
 };
 
 
+const filtraArray = useCallback(() => {
+  const aggregatedData30Days = aggregateData(citObjetos, 30, mesInicial);
+  const aggregatedData60Days = aggregateData(citObjetos, 60, mesInicial)
+  const aggregatedData90Days = aggregateData(citObjetos, 90, mesInicial)
+  const diasDoArrayMeses = arrayMeses.map(i => {
+    return i.dia;
+  });
+  let arrayPseNoGrafico2 = []
+  if(opcaoPeriodo == 0){
+    const arrayMesesEmNumeros = aggregatedData30Days[0].map((i, element) => {
+      return i.data;
+    });
+
+  
+    arrayPseNoGrafico2 = aggregatedData30Days[0].map((i, element) => {
+      return { x: element + 1, y: i.cit };
+    });
+  
+  }
+  if(opcaoPeriodo == 1){
+    const arrayMesesEmNumeros = aggregatedData60Days[0].map((i, element) => {
+      return i.data;
+    });
+
+  
+    arrayPseNoGrafico2 = aggregatedData60Days[0].map((i, element) => {
+      return { x: element + 1, y: i.cit };
+    });
+ }
+  if(opcaoPeriodo == 2){
+    const arrayMesesEmNumeros = aggregatedData90Days[0].map((i, element) => {
+      return i.data;
+    });
+    arrayPseNoGrafico2 = aggregatedData90Days[0].map((i, element) => {
+      return { x: element + 1, y: i.cit };
+    });
+ }
+ 
+  const arrayDatasMeses = [];
+  for (let i = mesInicial; i < arrayMeses.length; i++) {
+    const numeroMes = mapearMesParaNumero(arrayMeses[i].data);
+    arrayDatasMeses.push(`${diasDoArrayMeses[i]}/${numeroMes}`);
+  }
+
+  setArrayParametroX(arrayDatasMeses);
+  setArrayFiltrado(arrayPseNoGrafico2);
+}, [arrayMeses, mesInicial]);
+
+
 useEffect(() => {
-  if (mesSelecionado) {
+  const index = citObjetos.findIndex(item => item.data === mesSelecionado);
+  if (index !== -1) {
+    setMesInicial(index);
     filtraArray();
   }
-}, [mesSelecionado]);
+}, [mesSelecionado, filtraArray]);
 
+const mapearMesParaNumero = (mes) => {
+  const nomeMes = mes.split(" ")[0];
+  switch (nomeMes) {
+    case "Janeiro": return 1;
+    case "Fevereiro": return 2;
+    case "Março": return 3;
+    case "Abril": return 4;
+    case "Maio": return 5;
+    case "Junho": return 6;
+    case "Julho": return 7;
+    case "Agosto": return 8;
+    case "Setembro": return 9;
+    case "Outubro": return 10;
+    case "Novembro": return 11;
+    case "Dezembro": return 12;
+    default: return -1; // Mês não reconhecido
+  }
+}
 
 
 const dadosFiltrados = citObjetos.filter((item) => {
@@ -284,8 +341,6 @@ const aggregateData = (filteredData, interval, mesInicial) => {
   return aggregatedData;
 };
 
-const aggregatedData60Days = aggregateData(dadosFiltrados, 60);
-const aggregatedData90Days = aggregateData(dadosFiltrados, 90);
 
 const arrayPseNoGrafico =  arrayPse.map((element, i)=> {
   return {x: +i+1, y: element}
@@ -293,15 +348,12 @@ const arrayPseNoGrafico =  arrayPse.map((element, i)=> {
 })
 
 
-const filtraArray = () => {
-  let arrAux = []
-  const aggregatedData30Days = aggregateData(citObjetos, 30, mesInicial);
-  const arrayPseNoGrafico2 =  aggregatedData30Days[0].map((i, element)=> {
-    return {x: element+1, y: i.cit}
-  })  
-  arrAux = arrayPseNoGrafico2
-  setArrayFiltrado(arrAux)
-}
+
+const arrayBotaoSelect = arrayMeses.map(i => {return i.data})
+
+
+const arrayBotaoSelectSemRepeticoes = [...new Set(arrayBotaoSelect)]
+
 
 return (
         <ScrollView style={[estilo.corLightMenos1, style.container]}>
@@ -321,10 +373,17 @@ return (
                                     <Text style={[estilo.tituloH619px, estilo.textoCorSecundaria, estilo.centralizado, {marginTop: '3%'}]}>Evolução CIT {opcao == 0? "Diária" : opcao == 1 ? "Semanal" : "Mensal"}</Text>
 
                                   <View style={[estilo.centralizado, {width: '90%', marginTop: 10}]}>
-                                  <BotaoSelect     selecionado={true}  onChange={(value, index) => {handleSelectChange(value, index)}} titulo='Selecione um mês' max={1} options={arrayMeses}>
+                                  <BotaoSelect     selecionado={!arrayFiltrado[0].x == 0}  onChange={(value, index) => {handleSelectChange(value, index)}} titulo='Selecione um mês' max={1} options={arrayBotaoSelectSemRepeticoes}>
                       </BotaoSelect>
                                   </View>
-                        <VictoryChart theme={VictoryTheme.material}>
+                       {arrayFiltrado[0].x == 0? <View style={[{width: '90%', marginVertical: '5%'}, estilo.centralizado]}>
+                        <Text style={[estilo.tituloH619px, estilo.textoCorDanger, {textAlign: 'center'}]}>Selecione o mês do período inicial para renderizar os dados</Text>
+                          <View style={[estilo.centralizado, {marginVertical: '10'}]}>
+                          <Entypo name="line-graph" size={100} color="#FF6262" />
+
+                            </View>                        
+                         </View>:  
+                         <VictoryChart theme={VictoryTheme.material}>
                         <VictoryAxis
     style={{
       axisLabel: { fontSize: 5 }, // Adjust the font size of the axis label
@@ -357,25 +416,52 @@ return (
                                     data: { stroke: "#0066FF" },
                                     parent: { border: "1px solid #182128"},
                                 }}
-                                categories={opcao === 0? { x: diasPorOrdem } : opcao === 1? {x: citSemanalMapeado} : {x : arrayMeses}}
+                                categories={{x: arrayParametroX}}
 
                             data={opcao == 0 ? arrayFiltrado : opcao == 1 ?  arrayCitSemanalNoGrafico: arrayCitMensalNoGrafico} />            
-                    </VictoryChart>
+                    </VictoryChart>}
                     <View style={{marginLeft: '5%', marginBottom: '10%'}}>
                     <Text style={[estilo.textoP16px, estilo.textoCorSecundaria, style.Montserrat]}>Selecione o parâmetro que deseja visualizar a evolução:</Text>
                     <RadioBotao
                             options={['Diaria', 'Semanal', 'Mensal']}
                             selected={opcao}
-                            onChangeSelect={(opt, i) => { console.log(opcao); setOpcao(i);}}
+                            onChangeSelect={(opt, i) => { setOpcao(i);}}
                         >
                     </RadioBotao>
                     <Text style={[estilo.textoP16px, estilo.textoCorSecundaria, style.Montserrat]}>Período de tempo:</Text>
-                    <RadioBotao
+                    {opcao == 0?    
+                      <View>
+                        <RadioBotao
                             options={['30 dias', '60 dias', '90 dias']}
                             selected={opcaoPeriodo}
-                            onChangeSelect={(opt, i) => { console.log(opcaoPeriodo); setOpcaoPeriodo(i);}}
+                            onChangeSelect={(opt, i) => { setOpcaoPeriodo(i); console.log(i)}}
                         >
                     </RadioBotao>
+                    <Text style={[estilo.textoCorSecundaria, estilo.tituloH619px, {marginVertical: 10}]}>Valores:</Text>
+                    <View style={{flexDirection: 'row'}}>
+                    <View>
+                    {arrayParametroX.map((item) => {return <Text style={[estilo.textoCorSecundaria, estilo.textoP16px]}>Data: {item}</Text>})}    
+                      </View>
+                      <View>
+                      {arrayFiltrado.map((item) => {return <Text style={[estilo.textoCorSecundaria, estilo.textoP16px]}>- CIT: {item.y}</Text>})}
+
+                      </View>
+                    </View>
+
+                        </View>
+
+                    
+                    : opcao === 1?                     <RadioBotao
+                            options={['1 mês', '2 meses', '3 meses', '4 meses', '5 meses', '6 meses']}
+                            selected={opcaoPeriodo}
+                            onChangeSelect={(opt, i) => { setOpcaoPeriodo(i); console.log(i)}}
+                        >
+                    </RadioBotao> :                     <RadioBotao
+                            options={['24 barras']}
+                            selected={opcaoPeriodo}
+                            onChangeSelect={(opt, i) => { setOpcaoPeriodo(i); console.log(i)}}
+                        >
+                    </RadioBotao>}
                 </View>
                     </View>
                     ) : <ModalSemConexao ondeNavegar={'Home'} navigation={navigation}/>}
